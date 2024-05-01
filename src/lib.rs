a4271c19238379697fded39c59c46485c3c316a6
ThailanSwaity <th4lans@gmail.com>
2024-05-01 12:49:48 -0500

    Feat: implemented id search

--- src/lib.rs
+++ src/lib.rs
@@ -43,56 +43,65 @@ impl Query {
         query
     }
 
-    pub fn to_parameter_string(&self) -> String {
-        let mut parameter_string: String = "?".to_owned();
+    fn is_id_query(&self) -> bool {
+        !self.id.is_none()
+            && self.platform.is_none()
+            && self.sort_by.is_none()
+            && self.giveaway_type.is_none()
+    }
+
+    pub fn to_parameterized_url(&self) -> String {
+        let mut parameterized_url: String = "https://gamerpower.com/api/giveaway".to_owned();
+        if !self.is_id_query() {
+            parameterized_url.push_str("s?");
+        } else {
+            parameterized_url.push_str("?");
+        }
 
         let mut first_item = true;
         if let Some(id) = &self.id {
             if !first_item {
-                parameter_string.push_str("&");
+                parameterized_url.push_str("&");
             }
             first_item = false;
-            parameter_string.push_str("id=");
-            parameter_string.push_str(id);
+            parameterized_url.push_str("id=");
+            parameterized_url.push_str(id);
         }
         if let Some(platform) = &self.platform {
             if !first_item {
-                parameter_string.push_str("&");
+                parameterized_url.push_str("&");
             }
             first_item = false;
-            parameter_string.push_str("platform=");
-            parameter_string.push_str(platform);
+            parameterized_url.push_str("platform=");
+            parameterized_url.push_str(platform);
         }
         if let Some(giveaway_type) = &self.giveaway_type {
             if !first_item {
-                parameter_string.push_str("&");
+                parameterized_url.push_str("&");
             }
             first_item = false;
-            parameter_string.push_str("type=");
-            parameter_string.push_str(giveaway_type);
+            parameterized_url.push_str("type=");
+            parameterized_url.push_str(giveaway_type);
         }
         if let Some(sort_by) = &self.sort_by {
             if !first_item {
-                parameter_string.push_str("&");
+                parameterized_url.push_str("&");
             }
-            parameter_string.push_str("sort-by=");
-            parameter_string.push_str(sort_by);
+            parameterized_url.push_str("sort-by=");
+            parameterized_url.push_str(sort_by);
         }
 
-        parameter_string
+        parameterized_url
     }
 }
 
 pub fn run(query: Query) -> Result<(), Box<dyn Error>> {
-    let host_url = "https://gamerpower.com/api/giveaways";
+    let parameterized_url = query.to_parameterized_url();
     //    println!("{:#?}", &query);
 
-    let parameter_string = query.to_parameter_string();
-    let url = format!("{host_url}{parameter_string}");
+    //    println!("Full url string: {}", &parameterized_url);
 
-    //    println!("Full url string: {}", &url);
-
-    let body = reqwest::blocking::get(url)?.text()?;
+    let body = reqwest::blocking::get(parameterized_url)?.text()?;
     let json = json::parse(&body)?;
 
     print_query_json(json);
@@ -100,15 +109,24 @@ pub fn run(query: Query) -> Result<(), Box<dyn Error>> {
 }
 
 fn print_query_json(json: JsonValue) {
-    for i in 0..json.len() {
-        if json[i].is_null() {
-            continue;
+    if json.is_array() {
+        for i in 0..json.len() {
+            if json[i].is_null() {
+                continue;
+            }
+            println!(
+                "{}, {}, {}",
+                &json[i]["title"].to_string().bold(),
+                &json[i]["worth"].to_string().green(),
+                &json[i]["gamerpower_url"].to_string().purple()
+            );
         }
+    } else {
         println!(
             "{}, {}, {}",
-            &json[i]["title"].to_string().bold(),
-            &json[i]["worth"].to_string().green(),
-            &json[i]["gamerpower_url"].to_string().purple()
+            &json["title"].to_string().bold(),
+            &json["worth"].to_string().green(),
+            &json["gamerpower_url"].to_string().purple()
         );
     }
 }
